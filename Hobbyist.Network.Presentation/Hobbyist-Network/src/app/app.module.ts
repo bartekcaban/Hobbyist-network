d8bada544b8acdc7b59276c8d45fb799270efb6a
Bartłomiej Caban (bartcab990) <bartcab990@student.polsl.pl>
2019-10-24 14:28:40 +0200

    angular

--- Hobbyist.Network.Presentation/Hobbyist-Network/src/app/app.module.ts
+++ Hobbyist.Network.Presentation/Hobbyist-Network/src/app/app.module.ts
@@ -1,20 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './shared/material/material.module';
+
 import { LoginComponent } from './login/login.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { RegisterDialogComponent } from './login/components/register-dialog/register-dialog.component';
+import { LoginDialogComponent } from './login/components/login-dialog/login-dialog.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    NavbarComponent,
+    RegisterDialogComponent,
+    LoginDialogComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
